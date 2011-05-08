89c9e0c21829141214da1870e60876e0042ff6e1
Andrey Valyaev <dron@securitycode.ru>
2011-05-08 23:13:56 +0400

    Заработал туториал 3, он инклюдит стандартные файлы (не c++)

--- saffer.cpp
+++ saffer.cpp
@@ -1,15 +1,19 @@
 
 #include <iostream>
 
-#include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/Host.h>
+#include <llvm/Support/raw_ostream.h>
 
+#include <clang/Frontend/Utils.h>
+#include <clang/Frontend/FrontendOptions.h>
 #include <clang/Frontend/DiagnosticOptions.h>
+#include <clang/Frontend/PreprocessorOptions.h>
+#include <clang/Frontend/HeaderSearchOptions.h>
 #include <clang/Frontend/TextDiagnosticPrinter.h>
-#include <clang/Basic/FileSystemOptions.h>
+#include <clang/Basic/TargetInfo.h>
 #include <clang/Basic/FileManager.h>
 #include <clang/Basic/TargetOptions.h>
-#include <clang/Basic/TargetInfo.h>
+#include <clang/Basic/FileSystemOptions.h>
 #include <clang/Lex/HeaderSearch.h>
 #include <clang/Lex/Preprocessor.h>
 
@@ -35,10 +39,23 @@ int main(int argc, char **argv)
 	FileManager fileManager(fileSystemOptions);
 	SourceManager sourceManager(diagnostic, fileManager);
 	HeaderSearch headerSearch(fileManager);
-	
+
 	Preprocessor preprocessor(diagnostic, languageOptions, *pTargetInfo,
 		sourceManager, headerSearch);
 
+		
+	PreprocessorOptions preprocessorOptions;
+	preprocessorOptions.UsePredefines = false;
+
+	HeaderSearchOptions headerSearchOptions;
+	ApplyHeaderSearchOptions(headerSearch, headerSearchOptions,
+		languageOptions, pTargetInfo->getTriple());
+	
+	FrontendOptions frontendOptions;
+	
+	InitializePreprocessor(preprocessor, preprocessorOptions,
+		headerSearchOptions, frontendOptions);
+		
 	const FileEntry *pFile = fileManager.getFile(argv[1]);
 	sourceManager.createMainFileID(pFile);
 	preprocessor.EnterMainSourceFile();
