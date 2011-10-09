f0dbde6f7d259c8a016881edce8b80b77eaa0b9c
Andrey Valyaev <dron@securitycode.ru>
2011-10-09 23:27:18 +0300

    Наладил работоспособный чейнинг

--- CommandLine.cpp
+++ CommandLine.cpp
@@ -1,7 +1,8 @@
 
 #include <list>
 #include <vector>
-#include <boost/range/algorithm/find.hpp>
+#include <boost/bind.hpp>
+#include <boost/range/algorithm/find_if.hpp>
 
 using namespace std;
 using namespace boost;
@@ -11,7 +12,7 @@ vector<const char *> Chaining(int argc, const char **argv)
 	list<const char *> args(argv, argv + argc);
 	args.pop_front();
 	
-	list<const char *>::iterator chain = find(args, "-chain");
+	list<const char *>::iterator chain = find_if(args, bind(strcmp, _1, "-chain") == 0);
 	if (chain != args.end()) {
 		list<const char *>::iterator cexec = args.erase(chain);
 		if (cexec != args.end()) {
--- paranoid.cpp
+++ paranoid.cpp
@@ -1,14 +1,18 @@
 
 #include <unistd.h>
+#include <string>
 #include <vector>
+#include "CommandLine.h"
 
 using namespace std;
 
-int main(int argc, char **argv)
+int main(int argc, const char **argv)
 {
-	vector<const char *> args(argv, argv + argc);
-	args[0] = "gcc";
-	args.push_back(0);
+	vector<const char *> args = Chaining(argc, argv);
+
+	if (string(args[0]) == "gcc" || string(args[0]) == "g++") {
+		// anayze
+	}
 	
-	return execvp("gcc", const_cast<char **>(&args[0]));
+	return execvp(args[0], const_cast<char **>(&args[0]));
 }
