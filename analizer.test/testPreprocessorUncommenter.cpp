adcbaa924bf550dabac56e0fb0ab80d589422438
Andrey Valyaev <dron@securitycode.ru>
2011-10-15 23:47:56 +0300

    вынес CUSTOM_ASSERTIONS

--- analizer.test/testPreprocessorUncommenter.cpp
+++ analizer.test/testPreprocessorUncommenter.cpp
@@ -2,14 +2,24 @@
 #include <iostream>
 #include <list>
 #include <boost/bind.hpp>
+#include <boost/range/algorithm/transform.hpp>
 #include <boost/test/unit_test.hpp>
 #include <Line.h>
 #include <PreprocessorUncommenter.h>
 #include <Token.h>
+#include "Assertions.h"
 
 using namespace std;
+using namespace boost;
 
 BOOST_AUTO_TEST_SUITE(suitePreprocessorUncommenter)
+
+void CUSTOM_REQUIRE_EQUAL_TOKENS(const list<Token> &tokens, const list<string> &expected)
+{
+	list<string> values;
+	transform(tokens, back_inserter(values), [](const Token &t){ return t.getText(); });
+	CUSTOM_REQUIRE_EQUAL_COLLECTIONS(values, expected);
+}
 
 struct testLL {
 	list<Token> tokens;
@@ -26,8 +36,8 @@ BOOST_AUTO_TEST_CASE(testNoComment)
 	Line line(0, "1234567890", 0);
 	pu.parse(&line);
 	
-	BOOST_REQUIRE_EQUAL(ll.tokens.size(), 1);
-	BOOST_REQUIRE_EQUAL(ll.tokens.front().getText(), "1234567890");
+	list<string> expected = { "1234567890" };
+	CUSTOM_REQUIRE_EQUAL_TOKENS(ll.tokens, expected);
 }
 
 BOOST_AUTO_TEST_CASE(testSimpleComment)
@@ -38,8 +48,8 @@ BOOST_AUTO_TEST_CASE(testSimpleComment)
 	Line line(0, "12345//67890", 0);
 	pu.parse(&line);
 	
-	BOOST_REQUIRE_EQUAL(ll.tokens.size(), 1);
-	BOOST_REQUIRE_EQUAL(ll.tokens.front().getText(), "12345");
+	list<string> expected = { "12345" };
+	CUSTOM_REQUIRE_EQUAL_TOKENS(ll.tokens, expected);
 }
 
 BOOST_AUTO_TEST_CASE(testComentInQuote)
@@ -50,8 +60,8 @@ BOOST_AUTO_TEST_CASE(testComentInQuote)
 	Line line(0, "1234\"5//6\"7890", 0);
 	pu.parse(&line);
 	
-	BOOST_REQUIRE_EQUAL(ll.tokens.size(), 1);
-	BOOST_REQUIRE_EQUAL(ll.tokens.front().getText(), "1234\"5//6\"7890");
+	list<string> expected = { "1234\"5//6\"7890" };
+	CUSTOM_REQUIRE_EQUAL_TOKENS(ll.tokens, expected);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
