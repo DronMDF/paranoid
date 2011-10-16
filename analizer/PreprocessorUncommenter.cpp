8e77bac486613cf7e661d80661e324f3029df366
Andrey Valyaev <dron@securitycode.ru>
2011-10-16 23:05:50 +0300

    Блочный комментарий в одной строке обработал, но это не очень правильно

--- analizer/PreprocessorUncommenter.cpp
+++ analizer/PreprocessorUncommenter.cpp
@@ -1,11 +1,15 @@
 
 #include <string>
+#include <boost/algorithm/string/predicate.hpp>
 #include <boost/regex.hpp>
 #include "Line.h"
 #include "PreprocessorUncommenter.h"
 
 using namespace std;
 using namespace boost;
+
+// Чтобы не разбираться с тем, новая строка или страя - надо хранить состояние отдельно,
+// А объект создавать для каждой строки заново. Состояние может быть статическим.
 
 PreprocessorUncommenter::PreprocessorUncommenter(const PreprocessorUncommenter::low_parser_call &parser)
 	: ll_parser(parser)
@@ -20,11 +24,16 @@ void PreprocessorUncommenter::parse(const Line *line) const
 void PreprocessorUncommenter::scanComments(const Line *line, unsigned offset) const
 {
 	const string text = string(line->getText(), offset);
-	const regex re("[^\\\\](\".*[^\\\\]\"|//)");
+	const regex re("[^\\\\](\".*[^\\\\]\"|//|/\\*.*\\*/)");
 	smatch what;
 	if (regex_search(text, what, re)) {
 		if (what.str(1) == "//") {
 			ll_parser(line, 0, offset + what.position(1));
+		} else if (starts_with(what.str(1), "/*")) {
+			// Для начала просто поделим на два.
+			ll_parser(line, 0, offset + what.position(1));
+			ll_parser(line, offset + what.position(1) + what.length(1), 
+				  text.size() - (offset + what.position(1) + what.length(1)));
 		} else {
 			scanComments(line, offset + what.position(1) + what.length(1));
 		}
@@ -33,3 +42,4 @@ void PreprocessorUncommenter::scanComments(const Line *line, unsigned offset) co
 	
 	ll_parser(line, 0, line->getText().size());
 }
+
