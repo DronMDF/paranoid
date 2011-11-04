5f899ed31f8742bc305f684f782aa7122f09874f
Andrey Valyaev <dron@securitycode.ru>
2011-11-04 22:48:37 +0300

    Some TODOs again

--- analizer/Preprocessor/File.h
+++ analizer/Preprocessor/File.h
@@ -13,15 +13,19 @@ public:
 	typedef std::list<std::shared_ptr<const Line>> lines_type;
 	typedef lines_type::const_iterator const_iterator;
 
-	// TODO: get the Preprocessor pointer
+	// TODO: get the Preprocessor pointer and filename
 	explicit File(std::istream &in);
 	File() : lines() { }
 	
 	virtual ~File() { };
 
+	// TODO: Command for tokenizing
+	// TODO: Command for adding include point
+	
 	// TODO: Pull to Location interface
 	virtual std::string getLocation() const;
 	
+	// TODO: Need to be replaced by visitor interface
 	const_iterator begin() const;
 	const_iterator end() const;
 	
