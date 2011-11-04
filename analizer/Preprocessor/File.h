5f14d5c9ee3f347967b7e5291e15be600243849d
Andrey Valyaev <dron@securitycode.ru>
2011-11-04 22:41:58 +0300

    Ideas about class structure

--- analizer/Preprocessor/File.h
+++ analizer/Preprocessor/File.h
@@ -7,21 +7,26 @@
 
 class Line;
 
+// One file structure class
 class File {
 public:
 	typedef std::list<std::shared_ptr<const Line>> lines_type;
 	typedef lines_type::const_iterator const_iterator;
 
+	// TODO: get the Preprocessor pointer
 	explicit File(std::istream &in);
 	File() : lines() { }
 	
 	virtual ~File() { };
 
+	// TODO: Pull to Location interface
 	virtual std::string getLocation() const;
 	
 	const_iterator begin() const;
 	const_iterator end() const;
 	
 private:
+	// TODO: Keep the Preprocessor pointer for include callbacks
+	// TODO: Keep the preprocessed token list only by this file (not by included)
 	lines_type lines;
 };
--- analizer/Preprocessor/Preprocessor.h
+++ analizer/Preprocessor/Preprocessor.h
@@ -7,10 +7,12 @@
 class File;
 class Line;
 
+// Preprocessor is main class for parse raw source
 class Preprocessor {
 public:
 	typedef std::list<Token>::const_iterator const_iterator;
 
+	// TODO: construct with include/sysinclude paths and one source file name
 	explicit Preprocessor(const File &file);
 	virtual ~Preprocessor();
 
@@ -23,5 +25,8 @@ protected:
 	void addToken(Token token);
 	
 private:
+	// TODO: Keep map of files by full name
+	// TODO: Pass this in every file
+	
 	std::list<Token> tokens;
 };
