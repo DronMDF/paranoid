3814c2f30459f259632b04c4ce70c8fff41c58a2
Andrey Valyaev <dron@securitycode.ru>
2011-11-15 13:06:07 +0300

    Cleanup

--- analizer/Preprocessor/Tokenizer.cpp
+++ analizer/Preprocessor/Tokenizer.cpp
@@ -17,42 +17,40 @@ Tokenizer::Tokenizer(add_token_t add_token)
 	
 void Tokenizer::parse(const shared_ptr<const Line> &line) const
 {
-	parseRecurse(line, 0, 0);
+	parseRecurse(line);
 	add_token(shared_ptr<Token>(new TokenNewline(line)));
 }
 
-void Tokenizer::parseRecurse(const shared_ptr<const Line> &line, size_type begin __attribute__((unused)), size_type current __attribute__((unused))) const
+void Tokenizer::parseRecurse(const shared_ptr<const Line> &line) const
 {
 	for (size_type position = 0; position != string::npos && position < line->getText().size(); ) {
 		if (is_any_of(" \t")(line->getText()[position])) {
-			position = parseSpace(line, position, 0);
+			position = parseSpace(line, position);
 		} else if (line->getText()[position] == '"') {
-			position = parseString(line, position, 0);
+			position = parseString(line, position);
 		} else if (line->getText()[position] == '\'') {
-			position = parseChar(line, position, 0);
+			position = parseChar(line, position);
 		} else {
-			position = parseWord(line, position, 0);
+			position = parseWord(line, position);
 		}
 	}
 }
 
-Tokenizer::size_type Tokenizer::parseSpace(const shared_ptr<const Line> &line, size_type begin, size_type current __attribute__((unused))) const
+Tokenizer::size_type Tokenizer::parseSpace(const shared_ptr<const Line> &line, size_type begin) const
 {
 	const size_type end = line->getText().find_first_not_of(" \t", begin);
 	add_token(shared_ptr<Token>(new TokenSpace(line, begin, end)));
-	//parseRecurse(line, end, end);
 	return end;
 }
 
-Tokenizer::size_type Tokenizer::parseWord(const shared_ptr<const Line> &line, size_type begin, size_type current __attribute__((unused))) const
+Tokenizer::size_type Tokenizer::parseWord(const shared_ptr<const Line> &line, size_type begin) const
 {
 	const size_type end = line->getText().find_first_of(" \t", begin);
 	add_token(shared_ptr<Token>(new TokenWord(line, begin, end)));
-	//parseRecurse(line, end, end);
 	return end;
 }
 
-Tokenizer::size_type Tokenizer::parseString(const shared_ptr<const Line> &line, size_type begin, size_type current __attribute__((unused))) const
+Tokenizer::size_type Tokenizer::parseString(const shared_ptr<const Line> &line, size_type begin) const
 {
 	size_type end = begin + 1;
 	
@@ -72,11 +70,10 @@ Tokenizer::size_type Tokenizer::parseString(const shared_ptr<const Line> &line, 
 	
 	end += 1;
 	add_token(shared_ptr<Token>(new TokenWord(line, begin, end)));
-	//parseRecurse(line, end, end);
 	return end;
 }
 
-Tokenizer::size_type Tokenizer::parseChar(const shared_ptr<const Line> &line, size_type begin, size_type current __attribute__((unused))) const
+Tokenizer::size_type Tokenizer::parseChar(const shared_ptr<const Line> &line, size_type begin) const
 {
 	size_type end = begin + 1;
 	while(true) {
@@ -94,6 +91,5 @@ Tokenizer::size_type Tokenizer::parseChar(const shared_ptr<const Line> &line, si
 
 	end += 1;
 	add_token(shared_ptr<Token>(new TokenWord(line, begin, end)));
-	//parseRecurse(line, end, end);
 	return end;
 }
--- analizer/Preprocessor/Tokenizer.h
+++ analizer/Preprocessor/Tokenizer.h
@@ -18,11 +18,11 @@ public:
 private:
 	typedef std::string::size_type size_type;
 	
-	void parseRecurse(const std::shared_ptr<const Line> &line, size_type begin, size_type current) const;
-	size_type parseSpace(const std::shared_ptr<const Line> &line, size_type begin, size_type current) const;
-	size_type parseWord(const std::shared_ptr<const Line> &line, size_type begin, size_type current) const; 
-	size_type parseString(const std::shared_ptr<const Line> &line, size_type begin, size_type current) const;
-	size_type parseChar(const std::shared_ptr<const Line> &line, size_type begin, size_type current) const;
+	void parseRecurse(const std::shared_ptr<const Line> &line) const;
+	size_type parseSpace(const std::shared_ptr<const Line> &line, size_type begin) const;
+	size_type parseWord(const std::shared_ptr<const Line> &line, size_type begin) const; 
+	size_type parseString(const std::shared_ptr<const Line> &line, size_type begin) const;
+	size_type parseChar(const std::shared_ptr<const Line> &line, size_type begin) const;
 
 	add_token_t add_token;
 };
