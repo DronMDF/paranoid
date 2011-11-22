8fe8d6c01aeada5f7c71c2ae4ae94ac3f1ff8fe1
Andrey Valyaev <dron@securitycode.ru>
2011-11-22 17:01:33 +0300

    TODOs and stub for Location

--- analizer/Preprocessor/TokenList.cpp
+++ analizer/Preprocessor/TokenList.cpp
@@ -20,10 +20,18 @@ string TokenList::getText() const
 
 string TokenList::getLocation() const
 {
+	// TODO: Location agregator can accumulate line info from all token
+	if (!tokens.empty()) {
+		return tokens.front()->getLocation();
+	}
 	return string();
 }
 
 string TokenList::getTextInString(const string &begin_marker, const string &end_marker) const
 {
+	// TODO: Selection agregator can accumulate string from all token
+	if (!tokens.empty()) {
+		return tokens.front()->getTextInString(begin_marker, end_marker);
+	}
 	return string();
 }
