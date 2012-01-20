ce7652f28b79696a2e419e139f79208eb56947cc
Andrey Valyaev <dron@securitycode.ru>
2012-01-20 18:26:55 +0400

    Incomplete changes, to done

--- analizer/Preprocessor/ErrorFormatter.cpp
+++ analizer/Preprocessor/ErrorFormatter.cpp
@@ -1,11 +1,22 @@
 
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/range/algorithm/replace_copy_if.hpp>
 #include "ErrorFormatter.h"
 #include "Error.h"
 #include "Line.h"
 #include "Token.h"
 
+using namespace std;
+using boost::is_space;
+using boost::replace_copy_if;
+
 std::string ErrorFormatter(const Error &error)
 {
 	auto line = error.token->getLine();
-	return line->getLocation() + " error: " + error.message + "\n" + line->getText();
+	auto linetext = line->getText();
+	string tokenmask;
+	replace_copy_if(linetext, back_inserter(tokenmask), !is_space(), string(" "));
+	return line->getLocation() + " error: " + error.message + "\n" 
+		+ linetext + '\n' + tokenmask;
 }
