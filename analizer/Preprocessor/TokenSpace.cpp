28b9a45774909af9e737db2828052d37185804ee
Andrey Valyaev <dron@securitycode.ru>
2012-02-10 10:19:53 +0400

    TokenSpace is not a TokenWord

--- analizer/Preprocessor/TokenSpace.cpp
+++ analizer/Preprocessor/TokenSpace.cpp
@@ -1,15 +1,48 @@
 
+#include <boost/assert.hpp>
+#include "Line.h"
 #include "TokenSpace.h"
 
 using namespace std;
 
 TokenSpace::TokenSpace(const shared_ptr<const Line> &line, 
 		       string::size_type begin, string::size_type end)
-	: TokenWord(line, begin, end)
+	: line(line), begin(begin), end(end)
 {
+	BOOST_ASSERT(line);
+	if (this->end == string::npos) {
+		this->end = line->getText().size();
+	}
+	BOOST_ASSERT(begin < this->end);
+	BOOST_ASSERT(this->end <= line->getText().size());
 }
 
 string TokenSpace::getText() const
 {
 	return " ";
+}
+
+string TokenSpace::getLocation() const
+{
+	return line->getLocation();
+}
+
+string TokenSpace::getFileName() const
+{
+	return line->getFileName();
+}
+
+shared_ptr<const Line> TokenSpace::getLine() const
+{
+	return line;
+}
+
+size_t TokenSpace::getBeginPos() const
+{
+	return begin;
+}
+
+size_t TokenSpace::getEndPos() const
+{
+	return end;
 }
--- analizer/Preprocessor/TokenSpace.h
+++ analizer/Preprocessor/TokenSpace.h
@@ -1,11 +1,21 @@
 
 #pragma once
-#include "TokenWord.h"
+#include "Token.h"
 
-class TokenSpace : public TokenWord {
+class TokenSpace : public Token {
 public:
 	TokenSpace(const std::shared_ptr<const Line> &line, 
 		  std::string::size_type begin, std::string::size_type end);
 	
 	virtual std::string getText() const;
+	virtual std::string getLocation() const;
+	virtual std::string getFileName() const;
+	virtual std::shared_ptr<const Line> getLine() const;
+	virtual size_t getBeginPos() const;
+	virtual size_t getEndPos() const;
+
+private:
+	const std::shared_ptr<const Line> line;
+	std::string::size_type begin;
+	std::string::size_type end;
 };
