5dbfa187e414e3c073fc9c0cdfe31d0520d60bb5
Andrey Valyaev <dron@securitycode.ru>
2012-03-04 21:33:11 +0400

    Test prototype for IncludePath

--- analizer.test/Preprocessor/testIncludePath.cpp
+++ analizer.test/Preprocessor/testIncludePath.cpp
@@ -1,22 +1,43 @@
 
 #include <boost/test/unit_test.hpp>
+#include "../Assertions.h"
 
 using namespace std;
+
+// TODO: Move to production
+class IncludePath {
+public:
+	virtual ~IncludePath();
+	list<string> getQuotedPath() const { return {}; };
+	list<string> getSystemPath() const { return {}; };
+};
+
+IncludePath::~IncludePath() = default;
 
 BOOST_AUTO_TEST_SUITE(suiteIncludePath)
 
 BOOST_AUTO_TEST_CASE(testParseVerbose)
 {
+	struct testIncludePath : IncludePath {
+		const list<string> lines;
+		testIncludePath(const list<string> &lines) : lines(lines) { }
+		// TODO: Suspend invoking compiler
+	};
+	
 	// lines produced by gcc: 'echo | gcc -v -E -'
-	auto lines = {
+	testIncludePath tip({
 		"#include \"...\" search starts here:",
 		"#include <...> search starts here:",
 		"/usr/lib/gcc/i686-pc-linux-gnu/4.5.3/include",
 		"/usr/lib/gcc/i686-pc-linux-gnu/4.5.3/include-fixed",
 		"/usr/include",
-		"End of search list." };
+		"End of search list." });
 	
-	// TODO: parse this
+	CUSTOM_REQUIRE_EQUAL_COLLECTIONS(tip.getQuotedPath(), list<string>());
+	CUSTOM_REQUIRE_EQUAL_COLLECTIONS(tip.getSystemPath(), {
+		"/usr/lib/gcc/i686-pc-linux-gnu/4.5.3/include",
+		"/usr/lib/gcc/i686-pc-linux-gnu/4.5.3/include-fixed",
+		"/usr/include" });
 }
 
 BOOST_AUTO_TEST_SUITE_END()
