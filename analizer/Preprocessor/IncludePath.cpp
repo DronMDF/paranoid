8c84e0272c9bb098087fca8ac4aa94feac6b983f
Andrey Valyaev <dron@securitycode.ru>
2012-03-07 22:16:22 +0400

    GCC invoked

--- analizer/Preprocessor/IncludePath.cpp
+++ analizer/Preprocessor/IncludePath.cpp
@@ -7,10 +7,29 @@ IncludePath::~IncludePath() = default;
 
 list<string> IncludePath::readSpec() const 
 {
-	FILE *in = popen("gcc -v -E", "r");
+	FILE *in = popen("sh -c 'echo | gcc -v -E -' 2>&1", "r");
+	if (in == 0) {
+		return {};
+	}
+	
+	list<string> result;
+	string last;
+	while (true) {
+		char c = fgetc(in);
+		if (c == EOF) {
+			break;
+		}
+			
+		if (c == '\n') {
+			result.push_back(last);
+			last.erase();
+		} else {
+			last.push_back(c);
+		}
+	}
 	
 	pclose(in);
-	return {};
+	return result;
 }
 
 list<string> IncludePath::getQuotedPath() const 
@@ -20,5 +39,5 @@ list<string> IncludePath::getQuotedPath() const
 
 list<string> IncludePath::getSystemPath() const 
 {
-	return {};
+	return readSpec();
 }
