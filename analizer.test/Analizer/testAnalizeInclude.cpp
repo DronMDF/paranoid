299301c1b7bfd21e79cd5f19dd1977629bdba766
Andrey Valyaev <dron@securitycode.ru>
2012-03-21 23:01:52 +0400

    Test for unused

---
+++ analizer.test/Analizer/testAnalizeInclude.cpp
@@ -0,0 +1,23 @@
+
+#include <boost/test/unit_test.hpp>
+#include <Analizer/AnalizeInclude.h>
+#include <Preprocessor/TokenInclude.h>
+#include "../Assertions.h"
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(suiteAnalizeInclude)
+
+BOOST_AUTO_TEST_CASE(testTrivial)
+{
+	AnalizeInclude ai;
+	//auto file = make_shared<File>();
+	
+	auto include = make_shared<TokenInclude>(list<shared_ptr<const Token>>());
+	//include.include(file);
+	
+	//ai.checkToken(include);
+	CUSTOM_REQUIRE_EQUAL_COLLECTIONS(ai.getUnused(), {include});
+}
+
+BOOST_AUTO_TEST_SUITE_END()
