aa53f6b3369a6ec97bb51316528360c678a17b8d
Andrey Valyaev <dron@securitycode.ru>
2012-04-19 22:44:40 +0400

    Cleanup in TokenExpression

--- analizer/Preprocessor/TokenExpression.cpp
+++ analizer/Preprocessor/TokenExpression.cpp
@@ -5,79 +5,8 @@
 using namespace std; 
 
 TokenExpression::TokenExpression(const initializer_list<TokenPredicate> &expr)
-	: predicates(expr.begin(), expr.end()), position(0), quantity(0)
+	: predicates(expr.begin(), expr.end())
 {
-}
-
-unsigned TokenExpression::requiredPosition(const shared_ptr<const Token> &token, unsigned position) const
-{
-	if (predicates[position].isOptional() and position + 1 < predicates.size()) {
-		auto rpos = requiredPosition(token, position + 1);
-		if (rpos > position) {
-			return rpos;
-		}
-	}
-	
-	if (predicates[position](token)) {
-		return position;
-	}
-	
-	return 0;
-}
-
-// This is not good solution, mutable state :(
-// Need to keep state outside of expression...
-bool TokenExpression::match(const shared_ptr<const Token> &token)
-{
-	if (position >= predicates.size()) {
-		// This is successefull false
-		return false;
-	}
-	
-	auto rpos = requiredPosition(token, position);
-	if (rpos > 0) {
-		position = rpos;
-		quantity = 0;
-	}
-	
-	if (predicates[position](token)) {
-		quantity++;
-		if (!predicates[position].isSome()) {
-			position++;
-			quantity = 0;
-		}
-		
-		return true;
-	} else {
-		if (quantity > 0) {
-			position++;
-			quantity = 0;
-			return match(token);
-		}
-
-		if (predicates[position].isOptional()) {
-			position++;
-			return match(token);
-		}
-	}
-	
-	return false;
-}
-
-bool TokenExpression::isMatched() const
-{
-	unsigned last_position = position;
-	while (last_position < predicates.size() && predicates[last_position].isOptional()) {
-		last_position++;
-	}
-	
-	return last_position >= predicates.size();
-}
-
-void TokenExpression::reset()
-{
-	position = 0;
-	quantity = 0;
 }
 
 tuple<bool, TokenExpression::token_list_iterator> TokenExpression::match(
@@ -85,20 +14,6 @@ tuple<bool, TokenExpression::token_list_iterator> TokenExpression::match(
 {
 	return matchIn(begin, end, 0);
 }
-
-// opt	some	match	action
-// f	f	f	fail
-// f	t	f	mc ? tok++ : fail
-// t	t	f	mc ? recur(tok++, pred) or pred++ : tok++
-// t	f	f	pred++
-// 
-// t	f	t	recur(tok++, pred++) or pred++
-// f	f	t	tok++, pred++
-// t	t	t	mc++, recur(tok++, pred) or tok++
-// f	t	t	mc++, tok++
-// 
-// at pred end: success
-// at tok end: fail
 
 tuple<bool, TokenExpression::token_list_iterator> TokenExpression::matchIn(
 	const token_list_iterator &begin, const token_list_iterator &end, unsigned psi) const
@@ -122,7 +37,7 @@ tuple<bool, TokenExpression::token_list_iterator> TokenExpression::matchIn(
 			if (is_match or (!is_match and is_some and mc > 0)) {
 				// Check next or this predicate on next token
 				auto next = current;
-				auto result = matchIn(++next, end, (!is_some) ? i + 1 : i);
+				auto result = matchIn(++next, end, is_some ? i : i + 1);
 				if (get<0>(result)) {
 					return result;
 				}
--- analizer/Preprocessor/TokenExpression.h
+++ analizer/Preprocessor/TokenExpression.h
@@ -15,10 +15,6 @@ public:
 	TokenExpression() = delete;
 	TokenExpression(const std::initializer_list<TokenPredicate> &expr);
 	
-	bool match(const std::shared_ptr<const Token> &token);
-	bool isMatched() const;
-	void reset();
-	
 private:
 	typedef std::list<std::shared_ptr<Token>>::iterator token_list_iterator;
 	
@@ -27,10 +23,6 @@ public:
 		const token_list_iterator &end) const;
 private:
 	const std::vector<TokenPredicate> predicates;
-	unsigned position;
-	unsigned quantity;
-	
-	unsigned requiredPosition(const std::shared_ptr<const Token> &token, unsigned position) const;
 
 	std::tuple<bool, token_list_iterator> matchIn(const token_list_iterator &begin,
 		const token_list_iterator &end, unsigned psi) const;
