294bb0e9de284c6b9de0bd70b7cedfac385392ff
Andrey Valyaev <dron@securitycode.ru>
2012-05-02 22:59:08 +0400

    Detection of include guards

--- analizer/Analizer/ExpressionIfBlock.cpp
+++ analizer/Analizer/ExpressionIfBlock.cpp
@@ -1,5 +1,7 @@
 
+#include "ExpressionDefine.h"
 #include "ExpressionIfBlock.h"
+#include "ExpressionIfDirective.h"
 
 using namespace std;
 
@@ -22,5 +24,41 @@ set<string> ExpressionIfBlock::getDeclaredNames() const
 
 string ExpressionIfBlock::getIncludeGuardName() const
 {
-	return "";
+	auto cursor = tokens.begin();
+	
+	while (isSpace(*cursor)) {
+		++cursor;
+	}
+	
+	string macro;
+	if (auto iexp = dynamic_pointer_cast<const ExpressionIfDirective>(*cursor)) {
+		if (iexp->getDirective() == "#ifndef") {
+			auto used = iexp->getUsedNames();
+			if (used.size() == 1) {
+				macro = *used.begin();
+			}
+		}
+	} else {
+		return "";
+	}
+	
+	++cursor;
+	while (isSpace(*cursor) or isEol(*cursor)) {
+		++cursor;
+	}
+	
+	string define;
+	if (auto dexp = dynamic_pointer_cast<const ExpressionDefine>(*cursor)) {
+		auto declared = dexp->getDeclaredNames();
+		if (declared.size() == 1) {
+			define = *declared.begin();
+		}
+	}
+
+	if (macro.empty() or macro != define) {
+		return "";
+	}
+	
+	return macro;
 }
+
