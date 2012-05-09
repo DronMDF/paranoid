437ee8d0364d49b5be7ce2b4869350e24f1f25dc
Andrey Valyaev <dron@securitycode.ru>
2012-05-09 23:44:28 +0400

    Analyze include guard for duplicates

--- analizer/Analizer/AnalizeIncludeGuard.h
+++ analizer/Analizer/AnalizeIncludeGuard.h
@@ -7,14 +7,29 @@ template <typename ErrorInsertIterator>
 class AnalyzeIncludeGuard {
 private:
 	ErrorInsertIterator ei;
+	std::set<std::string> guarded;
+	
+	void checkToken(const shared_ptr<const Token> &token) {
+		if (auto ifblock = dynamic_pointer_cast<const ExpressionIfBlock>(token)) {
+			auto guard = ifblock->getIncludeGuardName();
+			if (!guard.empty()) {
+				if (guarded.count(guard) > 0) {
+					ei++ = Error(token, "Include guard already used");
+				}
+				guarded.insert(guard);
+			}
+		}
+	}
+	
 public:
 	AnalyzeIncludeGuard(ErrorInsertIterator ei) 
 		: ei(ei)
 	{
 	}
 	
-	void operator() (const std::shared_ptr<File> &file __attribute__((unused))) 
+	void operator() (const std::shared_ptr<File> &file) 
 	{
-	}	
+		file->forEachToken(bind(&AnalyzeIncludeGuard::checkToken, this, _1));
+	}
 };
 
