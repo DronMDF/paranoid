859a43ef2a9f7fa1728405ea9d020e731d606f79
Andrey Valyaev <dron@securitycode.ru>
2012-05-28 00:14:51 +0400

    Uninline AnalyzeIncludeGuard

--- analizer/Analizer/AnalizeIncludeGuard.h
+++ analizer/Analizer/AnalizeIncludeGuard.h
@@ -2,67 +2,29 @@
 #pragma once
 #include <functional>
 #include <list>
+#include <map>
 #include <memory>
-#include <Preprocessor/File.h>
-#include <Preprocessor/Error.h>
-#include <Preprocessor/ErrorNote.h>
-#include <Preprocessor/ErrorList.h>
-#include "ExpressionIfBlock.h"
 
-// TODO: uninline
+class Token;
+class File;
+class Error;
+class ErrorList;
+
 class AnalyzeIncludeGuard {
+public:
+	AnalyzeIncludeGuard(ErrorList *el);
+	AnalyzeIncludeGuard(const AnalyzeIncludeGuard &parent);
+	
+	void operator() (const std::shared_ptr<File> &file);
+
 private:
 	ErrorList *el;
 	std::map<std::string, std::shared_ptr<const Token>> guarded;
 	bool is_guarded;
 	std::list<Error> current;
 	
-	void checkToken(const std::shared_ptr<const Token> &token) {
-		if (isSpace(token) or isEol(token)) {
-			return;
-		}
-
-		if (auto ifblock = std::dynamic_pointer_cast<const ExpressionIfBlock>(token)) {
-			auto guard = ifblock->getIncludeGuardName();
-			if (!guard.empty()) {
-				if (guarded.count(guard) > 0) {
-					current.push_back(Error(token, "Include guard already used"));
-					current.push_back(ErrorNote(guarded[guard], "Previously declared here"));
-				} else {
-					guarded[guard] = token;
-				}
-				return;
-			}
-		}
-		
-		is_guarded = false;
-	}
+	void checkToken(const std::shared_ptr<const Token> &token);
 	
-	AnalyzeIncludeGuard &operator=(const AnalyzeIncludeGuard&) = delete;
-	
-public:
-	AnalyzeIncludeGuard(ErrorList *el) 
-		: el(el), guarded(), is_guarded(true), current()
-	{
-	}
-
-	AnalyzeIncludeGuard(const AnalyzeIncludeGuard &parent) 
-		: el(parent.el), guarded(), is_guarded(true), current()
-	{
-	}
-	
-	void operator() (const std::shared_ptr<File> &file) 
-	{
-		is_guarded = true;
-		current.clear();
-		
-		file->forEachToken(std::bind(&AnalyzeIncludeGuard::checkToken, this, std::placeholders::_1));
-		
-		if (is_guarded) {
-			BOOST_FOREACH(const auto &err, current) {
-				el->insert(err);
-			}
-		}
-	}
+	AnalyzeIncludeGuard &operator=(const AnalyzeIncludeGuard&) = delete;	
 };
 
